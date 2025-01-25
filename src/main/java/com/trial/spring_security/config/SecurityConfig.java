1d7ef190a9d6994fd5b8f3aacca992ba9db588bd
khaidar-ar <96arrijal@gmail.com>
2025-01-25 22:06:24 +0700

    feat : remove userdetails bean

--- src/main/java/com/trial/spring_security/config/SecurityConfig.java
+++ src/main/java/com/trial/spring_security/config/SecurityConfig.java
@@ -17,17 +17,6 @@ public class SecurityConfig {
 
     private final CustomAuthProvider customAuthProvider;
 
-    @Bean
-    UserDetailsService userDetailsService() {
-        var user = User
-                .withUsername("user-1")
-                .password("{noop}12345")
-                .authorities("READ")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
         http.authenticationProvider(customAuthProvider);
