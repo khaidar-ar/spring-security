f34b67a14a383bfd37d4009826a24d55872b8306
khaidar-ar <96arrijal@gmail.com>
2025-01-25 21:57:53 +0700

    feat : configure provider

--- src/main/java/com/trial/spring_security/config/SecurityConfig.java
+++ src/main/java/com/trial/spring_security/config/SecurityConfig.java
@@ -1,21 +1,26 @@
 package com.trial.spring_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Configuration
 public class SecurityConfig {
 
+    private final CustomAuthProvider customAuthProvider;
+
     @Bean
     UserDetailsService userDetailsService() {
-        UserDetails user = User
+        var user = User
                 .withUsername("user-1")
                 .password("{noop}12345")
                 .authorities("READ")
@@ -26,12 +31,12 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(customAuthProvider);
         http.authorizeHttpRequests(
                 c -> {
                     c.requestMatchers("/java").authenticated();
                     c.requestMatchers("/hello").permitAll();
                 });
-
         return http.build();
     }
 }
