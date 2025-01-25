3130e12fca846c5b4c7448921168668c959302b6
khaidar-ar <96arrijal@gmail.com>
2025-01-25 22:04:43 +0700

    feat : remove code for chap-2

--- src/main/java/com/trial/spring_security/config/SecurityConfig.java
+++ src/main/java/com/trial/spring_security/config/SecurityConfig.java
@@ -9,13 +9,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 @Configuration
 public class SecurityConfig {
-
-    private final CustomAuthProvider customAuthProvider;
 
     @Bean
     UserDetailsService userDetailsService() {
@@ -30,12 +25,17 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults());
-        http.authenticationProvider(customAuthProvider);
         http.authorizeHttpRequests(
                 c -> {
                     c.requestMatchers("/java").authenticated();
                     c.requestMatchers("/hello").permitAll();
                 });
+        var user = User.withUsername("user-2")
+                .password("{noop}12345")
+                .authorities("READ")
+                .build();
+        var ims = new InMemoryUserDetailsManager(user);
+        http.userDetailsService(ims);
         return http.build();
     }
 }
