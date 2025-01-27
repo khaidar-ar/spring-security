7c111f1a3844d2b9fd13a090afdde5368ac1d55e
khaidar-ar <96arrijal@gmail.com>
2025-01-27 20:25:09 +0700

    feat : create user service

---
+++ src/main/java/com/trial/spring_security/service/UserService.java
@@ -0,0 +1,26 @@
+package com.trial.spring_security.service;
+
+import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+// @Service
+public class UserService implements UserDetailsService {
+
+    private final List<UserDetails> users;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return users.stream()
+                .filter(u -> u.getUsername().equals(username))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("username " + username + " not found"));
+    }
+
+}
