effe9feb25dc1ca1c1f240984c98da761b5ed703
khaidar-ar <96arrijal@gmail.com>
2025-01-28 20:16:45 +0700

    feat : add form login auth

--- src/main/java/com/trial/spring_security/config/SecurityConfig.java
+++ src/main/java/com/trial/spring_security/config/SecurityConfig.java
@@ -1,20 +1,12 @@
 package com.trial.spring_security.config;
 
-import java.io.IOException;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -27,18 +19,20 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(c -> {
-            c.realmName("CUSTOM_ENTRY_POINT");
-            c.authenticationEntryPoint(new AuthenticationEntryPoint() {
+        // http.httpBasic(c -> {
+        // c.realmName("CUSTOM_ENTRY_POINT");
+        // c.authenticationEntryPoint(new AuthenticationEntryPoint() {
 
-                @Override
-                public void commence(HttpServletRequest request, HttpServletResponse response,
-                        AuthenticationException authException) throws IOException, ServletException {
-                    response.addHeader("message", "custom entry point");
-                    response.sendError(HttpStatus.UNAUTHORIZED.value());
-                }
-            });
-        })
+        // @Override
+        // public void commence(HttpServletRequest request, HttpServletResponse
+        // response,
+        // AuthenticationException authException) throws IOException, ServletException {
+        // response.addHeader("message", "custom entry point");
+        // response.sendError(HttpStatus.UNAUTHORIZED.value());
+        // }
+        // });
+        // })
+        http.formLogin(Customizer.withDefaults())
                 .authenticationProvider(authenticationProvider)
                 .authorizeHttpRequests(c -> c.anyRequest().authenticated());
         return http.build();
