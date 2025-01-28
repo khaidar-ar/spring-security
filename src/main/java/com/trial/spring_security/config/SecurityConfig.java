5b528bc23bd6caba1f429a67ff48e23c4785620a
khaidar-ar <96arrijal@gmail.com>
2025-01-28 12:01:53 +0700

    feat : add static auth on filter chain

--- src/main/java/com/trial/spring_security/config/SecurityConfig.java
+++ src/main/java/com/trial/spring_security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.trial.spring_security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,16 +9,22 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 
 import com.trial.spring_security.filter.AuthLoggingFilter;
 import com.trial.spring_security.filter.RequestValidationFilter;
+import com.trial.spring_security.filter.StaticAuthFilter;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Configuration
 public class SecurityConfig {
+
+    private final StaticAuthFilter staticAuthFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.addFilterBefore(
-                new RequestValidationFilter(),
+                staticAuthFilter,
                 BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthLoggingFilter(), BasicAuthenticationFilter.class)
+                // .addFilterAfter(new AuthLoggingFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests(c -> c.anyRequest().permitAll());
         return http.build();
     }
