4ad35c972ba2fb531b4594cc0aef2a71797ded95
khaidar-ar <96arrijal@gmail.com>
2025-01-28 12:51:15 +0700

    fix : remove unused dependency

--- src/main/java/com/trial/spring_security/config/SecurityConfig.java
+++ src/main/java/com/trial/spring_security/config/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.trial.spring_security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
-import com.trial.spring_security.filter.AuthLoggingFilter;
-import com.trial.spring_security.filter.RequestValidationFilter;
 import com.trial.spring_security.filter.StaticAuthFilter;
 
 import lombok.AllArgsConstructor;
