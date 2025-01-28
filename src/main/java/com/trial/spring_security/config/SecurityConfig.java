4ea891999a19e78e479b4c436e18f6ac5948ace9
khaidar-ar <96arrijal@gmail.com>
2025-01-28 12:46:14 +0700

    feat : add filter chain using custom provider

--- src/main/java/com/trial/spring_security/config/SecurityConfig.java
+++ src/main/java/com/trial/spring_security/config/SecurityConfig.java
@@ -1,15 +1,11 @@
 package com.trial.spring_security.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import com.trial.spring_security.filter.AuthLoggingFilter;
-import com.trial.spring_security.filter.RequestValidationFilter;
-import com.trial.spring_security.filter.StaticAuthFilter;
 
 import lombok.AllArgsConstructor;
 
@@ -17,16 +13,14 @@ import lombok.AllArgsConstructor;
 @Configuration
 public class SecurityConfig {
 
-    private final StaticAuthFilter staticAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.addFilterBefore(
-                staticAuthFilter,
-                BasicAuthenticationFilter.class)
-                // .addFilterAfter(new AuthLoggingFilter(), BasicAuthenticationFilter.class)
-                .authorizeHttpRequests(c -> c.anyRequest().permitAll());
-        return http.build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.httpBasic(Customizer.withDefaults())
+                .authenticationProvider(authenticationProvider)
+                .authorizeHttpRequests(c -> c.anyRequest().authenticated())
+                .build();
     }
 
 }
