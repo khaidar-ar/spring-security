0718ff7de89ff2d0946c650ff906f74cbc747e7a
khaidar-ar <96arrijal@gmail.com>
2025-01-28 10:31:26 +0700

    feat : modify user service

--- src/main/java/com/trial/spring_security/service/UserService.java
+++ src/main/java/com/trial/spring_security/service/UserService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
 
