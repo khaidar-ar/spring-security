26ec4f7ee5856676ea796e72663c0ffdd6c618c3
khaidar-ar <96arrijal@gmail.com>
2025-01-29 20:20:36 +0700

    feat : configure cors

--- src/main/java/com/trial/spring_security/config/SecurityConfig.java
+++ src/main/java/com/trial/spring_security/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.httpBasic(Customizer.withDefaults())
-                .addFilterBefore(new CsrfTokenLogger(), CsrfFilter.class)
+                .csrf(c -> c.disable())
                 .authorizeHttpRequests(c -> c.anyRequest().permitAll());
         return http.build();
     }
--- src/main/java/com/trial/spring_security/controller/HelloController.java
+++ src/main/java/com/trial/spring_security/controller/HelloController.java
@@ -1,25 +1,29 @@
 package com.trial.spring_security.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello() {
-        SecurityContext ctx = SecurityContextHolder.getContext();
-        Authentication auth = ctx.getAuthentication();
-        return "hello " + auth.getName();
+        log.info("request from root");
+        return "main.html";
     }
 
-    @PostMapping("/hello")
+    @PostMapping("/test")
+    @CrossOrigin("http://localhost:8080")
+    @ResponseBody
     public String java() {
-        return "Hello from post method";
+        log.info("request from test");
+        return "test endpoint";
     }
 
 }
